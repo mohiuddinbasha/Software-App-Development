d25cae7a126322ceea76ccc25c0c17d609a822c0
sarvani97 <saisreesarvani@msitprogram.net>
2020-04-23 11:44:48 +0530

    changes for book page

--- Week-3/Project 1/models.py
+++ Week-3/Project 1/models.py
@@ -9,6 +9,20 @@ class User(db.Model):
     password = db.Column(db.String, nullable=False)
     timestamp = db.Column(db.Integer, nullable=False)
 
+class Book(db.Model):
+    __tablename__ = "books"
+    isbn = db.Column(db.String, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    author = db.Column(db.String, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+
+class Review(db.Model):
+    __tablename__ = "reviews"
+    isbn = db.Column(db.String, primary_key=True)
+    id = db.Column(db.String, nullable=False)
+    rating = db.Column(db.String, nullable=False)
+    comments = db.Column(db.String, nullable=False)
+
 
 
 # time.ctime(calendar.timegm(time.gmtime()))
--- Week-3/Project 1/sample.py
+++ Week-3/Project 1/sample.py
@@ -1,10 +1,10 @@
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+# db = SQLAlchemy()
 
-class Book(db.Model):
-    __tablename__ = "books"
-    isbn = db.Column(db.String, primary_key=True)
-    title = db.Column(db.String, nullable=False)
-    author = db.Column(db.String, nullable=False)
-    year = db.Column(db.Integer, nullable=False)
+# class Book(db.Model):
+#     __tablename__ = "books"
+#     isbn = db.Column(db.String, primary_key=True)
+#     title = db.Column(db.String, nullable=False)
+#     author = db.Column(db.String, nullable=False)
+#     year = db.Column(db.Integer, nullable=False)
